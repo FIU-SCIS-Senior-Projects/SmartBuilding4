115e4208f1f01f389d30c9b7268df67a2b3a11ac
evina003 <evina003@fiu.edu>
2016-02-28 14:39:45 -0500

    Notification Feature Added; Main Project Updated
    Imported all packages, finished feature project, notification system.

---
+++ FinishedFeatures/Sprint#3/NotificationSystemFeature/Assets/Scripts/Notification System/NotificationView.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NotificationView : MonoBehaviour 
+{
+
+	[SerializeField]CanvasGroup notificationPanel;
+	[SerializeField]Text questDetails;
+	[SerializeField]Button[] questInfo;
+	bool enabledWindow;
+
+	#region Unity 
+	// Use this for initialization
+	void Start () {
+	
+		enabledWindow = false;
+		notificationPanel.alpha = 0;
+	}
+	#endregion
+
+	#region Custom Methods
+	public void ViewQuestInfo(int i )
+	{
+		//questInfo[i]
+	}
+
+	public void EnableNotificationPanel()
+	{
+		enabledWindow = true;
+		StartCoroutine(PanelFade());
+	}
+
+	public void DisableNotificationPanel()
+	{
+		enabledWindow = false;
+		StartCoroutine(PanelFade());
+	}
+
+	IEnumerator PanelFade()
+	{
+		float targetAlpha = enabledWindow ? 1 : 0;
+		int modifier = enabledWindow ? 1: -1;
+
+		while((notificationPanel.alpha < targetAlpha && enabledWindow)
+			|| (notificationPanel.alpha > targetAlpha && !enabledWindow))
+		{
+			notificationPanel.alpha += Time.deltaTime * modifier;
+			yield return null;
+		}
+
+		yield return null;
+	}
+	#endregion
+
+	#region Getters/Setters
+	public bool WindowState
+	{
+		get{return enabledWindow;}
+		set{enabledWindow = value;}
+	}
+
+	public string QuestDetails
+	{
+		get{return questDetails.text;}
+		set{questDetails.text = value;}
+	}
+	#endregion
+}
---
+++ MainProject/Assets/Scripts/Notification System/NotificationController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotificationController : MonoBehaviour
+{
+	[SerializeField]NotificationView view;
+	[SerializeField]NotificationModel model;
+	bool[] activeQuest;
+
+	void Start()
+	{
+		activeQuest = new bool[model.Size];
+
+	}
+
+	void Update()
+	{
+		if(view.WindowState && Input.GetKeyUp(KeyCode.Space))
+		{
+			view.DisableNotificationPanel();
+		}	
+	}
+
+	public void SetActiveQuest(int i)
+	{
+		if(!activeQuest[i] && !view.WindowState)
+		{
+			activeQuest[i] = true;
+			view.QuestDetails = "The Emission Level in " + model.GetRoomName(i) + " is getting High: " + model.GetEmission(i);
+			view.EnableNotificationPanel();
+		}
+	}
+}
