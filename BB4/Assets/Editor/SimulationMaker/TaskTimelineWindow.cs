445872b1442d2a1dda192c9e01795fb7ef1351cb
Justin Fletcher <jflet013@fiu.edu>
2016-03-25 10:42:28 -0400

    worked on simulation maker

--- BB4/Assets/Editor/SimulationMaker/TaskTimelineWindow.cs
+++ BB4/Assets/Editor/SimulationMaker/TaskTimelineWindow.cs
@@ -8,9 +8,9 @@ using UnityEditor;
 
 public class TaskTimelineWindow : EditorWindow {
 
-	[MenuItem("Simulation Maker/Test")]
+	[MenuItem("Simulation Maker/Simulation Task Timeline")]
 	public static void OpenWindow() {
-		EditorWindow.GetWindow(typeof(TaskTimelineWindow), false, "TaskTimeLine");
+		EditorWindow.GetWindow(typeof(TaskTimelineWindow), false, "Task TimeLine");
 	}
 
 	public static string dbFilePath = "Assets/Editor/SimulationMaker/SimulationDatabase.asset";
@@ -29,12 +29,24 @@ public class TaskTimelineWindow : EditorWindow {
 		AssetDatabase.Refresh();
 	}
 
+
+
+	DatabaseSimulation selectedSimulation;
+	Task selectedTask;
+
+
+
 	void OnGUI() {
 
 		db = AssetDatabase.LoadAssetAtPath<SimulationTaskDatabase>(dbFilePath);
 
 		if (db != null) {
 
+			drawTopMenu();
+
+			drawTimeLine();
+
+			drawTasksOnTimeLine();
 
 		}
 
@@ -45,6 +57,36 @@ public class TaskTimelineWindow : EditorWindow {
 
 	}
 
+
+	void drawTopMenu() {
+		BeginWindows();
+
+		Vector2 size = new Vector2(Screen.width-20, 10);
+		Vector2 pos = Vector2.zero;
+		Rect rect = new Rect(pos.x, pos.y, size.x, size.y);
+		GUILayout.Window(1, rect, drawTopMenuControls, "");
+
+		EndWindows();
+	}
+
+
+	void drawTopMenuControls(int id) {
+
+
+	}
+
+
+
+
+	void drawTimeLine() {
+
+	}
+
+
+
+	void drawTasksOnTimeLine() {
+
+	}
 
 
 }
