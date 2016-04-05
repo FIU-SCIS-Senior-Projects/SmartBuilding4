1a25f0a28f566a541729f82c2e417f97ba5ee6d0
evina003 <evina003@fiu.edu>
2016-04-05 01:35:37 -0400

    First Person Character & Light Switch Controller
    Add Light Switch and firstperson fix

--- BB4/Assets/Scripts/LightEmission/EmissionView.cs
+++ BB4/Assets/Scripts/LightEmission/EmissionView.cs
@@ -4,33 +4,39 @@ using System.Collections;
 
 public class EmissionView : MonoBehaviour
 {
-	[SerializeField]Light spotLight;
-	[SerializeField]Button button;
+	[SerializeField]Light[] spotLight;
+	[SerializeField]Text button;
 
 	bool _isOn = true;
 	Text _buttonText;
 
 	void Start ()
 	{
-		_buttonText = button.GetComponentInChildren<Text> ();
+		_buttonText = button;
 	}
-	// Update is called once per frame
+		// Update is called once per frame
 	void Update () 
 	{
 		if (_isOn) {
-			_buttonText.text = "OFF";
+			_buttonText.text = "TURN OFF";
 		}
 		else {
-			_buttonText.text = "ON";
+			_buttonText.text = "TURN ON";
+			_buttonText.gameObject.SetActive (true);
 		}
+	}
+
+	public void TextState(bool state){
+		_buttonText.gameObject.SetActive (state);
 	}
 
 	public void ToggleLight()
 	{
 		_isOn = !_isOn;
-		spotLight.enabled = _isOn;
+		foreach(Light l in spotLight)
+			l.enabled = _isOn;
 	}
-
+		
 	public bool LightIsOn
 	{
 		get{return _isOn;}
