2612a4527d45587d5682c0bc9b85b4f6622e8d40
evina003 <evina003@fiu.edu>
2016-04-18 14:45:28 -0400

    Added Buggy Interaction

---
+++ BB4/Assets/Scripts/InteractionwithObject/InteractionController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionController : MonoBehaviour {
+
+	bool nearObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(NearObject)
+		{
+			InteractionControl();
+		}
+	}
+
+	public bool NearObject
+	{
+		get{ return nearObject;}
+		set{nearObject = value;}
+	}
+
+	public void InteractionControl()
+	{
+		
+	}
+}
