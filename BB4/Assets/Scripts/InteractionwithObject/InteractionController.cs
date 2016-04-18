990d152ba7c66c3e1a95c2935442c98eb185ae3b
evina003 <evina003@fiu.edu>
2016-04-18 15:55:28 -0400

    Fixed Interaction Bugs

--- BB4/Assets/Scripts/InteractionwithObject/InteractionController.cs
+++ BB4/Assets/Scripts/InteractionwithObject/InteractionController.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class InteractionController : MonoBehaviour {
 
-	bool nearObject;
-
-	// Use this for initialization
-	void Start () {
-	
+	bool nearObject = false;
+	bool carrying = false;
+	Camera mainCam;
+	[SerializeField]InteractionView view;
+	InteractionObject selectedObject;
+	Rigidbody carryingObject;
+	[SerializeField]float distance;
+	void Start()
+	{
+		mainCam = GetComponent<Camera>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
-		if(NearObject)
+		if(NearObject || carrying)
 		{
 			InteractionControl();
 		}
@@ -25,8 +30,59 @@ public class InteractionController : MonoBehaviour {
 		set{nearObject = value;}
 	}
 
+	public InteractionObject Selected
+	{
+		get{return selectedObject;}
+		set{selectedObject = value;}
+	}
+
 	public void InteractionControl()
 	{
-		
+		if(carrying)
+		{
+			Carry(carryingObject);
+		}
+
+		else
+		{
+			PickUp();
+		}
+
+	}
+
+	void PickUp()
+	{
+		if(Input.GetKeyDown(KeyCode.P))//Input.GetKey("PickUp"))
+		{
+			float x = Screen.width /2; 
+			float y = Screen.height/2;
+
+			Ray ray = mainCam.ScreenPointToRay(new Vector3(x,y));
+			RaycastHit hit;
+
+			if(Physics.Raycast(ray, out hit))
+			{
+				selectedObject = hit.collider.GetComponent<InteractionObject>();
+				if(selectedObject != null)
+				{
+					carrying = true;
+					carryingObject = selectedObject.RBody;
+				}
+			}
+
+		}
+	}
+
+	void Carry(Rigidbody x)
+	{
+		x.isKinematic = true;
+		x.transform.position = Vector3.Lerp(x.transform.position, transform.position + transform.forward * distance, Time.time);
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			x.isKinematic = false;
+			carrying = false; 
+			carryingObject = null;
+			selectedObject = null;
+		}
 	}
 }
